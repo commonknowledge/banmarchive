ed871ebd320331601a3f25c89a0dc86fabd9ef04
Chris Devereux <chris.devereux@pm.me>
2021-07-20 17:17:31 +0000

    publication page mobile responsive

--- helpers/content.py
+++ helpers/content.py
@@ -6,10 +6,10 @@ from django.db import models
 def get_children_of_type(parent, *types):
     if len(types) == 1:
         t = types[0]
-        return t.objects.live().child_of(parent)
+        return t.objects.live().child_of(parent).specific()
 
     content_types = tuple(ContentType.objects.get_for_model(t) for t in types)
-    return parent.get_children().filter(content_type__in=content_types)
+    return parent.get_children().filter(content_type__in=content_types).specific()
 
 
 def random_model(qs, count=None):
