8b6c7237e5e913fac79ef3938e1adbfd1c64ad5b
Chris Devereux <chris.devereux@pm.me>
2022-01-20 19:39:22 +0000

    trim down release script a bit

--- .bin/release.sh
+++ .bin/release.sh
@@ -2,7 +2,5 @@ set -e
 
 python manage.py migrate
 python manage.py preseed_transfer_table auth wagtailcore wagtailimages.image wagtaildocs
-python manage.py regenerate_thumbnails
-python manage.py keyword_extract
 python manage.py reindex_pdfs
 python manage.py update_index
