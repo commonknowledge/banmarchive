59e8895edc57a4152f4efdcf915bc80cf1c82023
Chris Devereux <chris.devereux@pm.me>
2022-01-21 16:43:38 +0000

    adds lighter upload script for part-automated ingest

---
+++ scripts/importutil.py
@@ -0,0 +1,52 @@
+
+import imp
+from sys import stderr, stdout
+from bs4 import BeautifulSoup, element
+from urllib.parse import unquote
+
+
+def emit(x=''):
+    if not x:
+        return
+
+    if hasattr(x, 'getText'):
+        return emit(x.getText())
+
+    x = ' '.join(
+        w.strip()
+        for w in x.split('\n')
+        if w
+    )
+    x = unquote(x)
+
+    stdout.write(f'"{x.strip()}",')
+
+
+def tostring(x):
+    if x is None:
+        return ''
+
+    return x if isinstance(x, str) else x.getText()
+
+
+def soundings():
+    with open('website/collections/soundings/index.htm') as fd:
+        bs = BeautifulSoup(fd)
+
+    for node in bs.select('.headerlarge, a'):
+        emit(node)
+        if node.name == 'a':
+            emit(node.attrs.get('href'))
+
+            nextel = node.next_sibling
+            authors = ''
+            while nextel and not (isinstance(nextel, element.Tag) and nextel.name == 'a'):
+                authors = authors + tostring(nextel)
+                nextel = nextel.next_sibling
+
+            emit(authors.replace(' and ', ', '))
+
+        print()
+
+
+soundings()
